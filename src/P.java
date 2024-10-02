74de4b63c26bcd943491ddc7923fcd2999306e37
DevilInDisguiise <juusafi@gmail.com>
2024-10-02 17:57:25 -0400

    First commit from new branch

---
+++ src/P.java
@@ -0,0 +1,87 @@
+import org.w3c.dom.ls.LSOutput;
+import java.util.Scanner;
+
+//class Vehicle {
+//    protected String brand="Ford";
+//    void carsound()
+//    {
+//        System.out.println("honk honk");
+//    }
+//    public void carbuyear()
+//    {
+//        System.out.println("2024");
+//    }
+//}
+//class Car extends Vehicle{
+//    public void caryear()
+//    {
+//        System.out.println("2018");
+//    }
+//    public void carsound()
+//    {
+//        System.out.println("poo poo");
+//    }
+//    public static void main(String[] args) {
+//        String carmodel="Mustang";
+//        Car mycar=new Car();
+//        mycar.carsound();
+//        System.out.println(mycar.brand+" = "+carmodel);
+//        mycar.carsound();
+//        Vehicle mecar=new Car();
+//        mecar.carbuyear();
+//
+//    }
+//}
+
+
+
+
+
+
+
+//##########################
+//        Arrays
+//##########################
+//class Array{
+//    public static void main(String[] args) {
+//        int[][]  rank={{1,2},{5,3},{4}};
+//        for (int i=0;i<rank.length;i++)
+//        {
+//            for (int j=0;j<rank[i].length;j++)
+//            {
+//                System.out.println(rank[i][j]);
+//            }
+//        }
+//
+//
+//    }
+//}
+
+
+
+
+
+//##########################
+//        Encapsulation
+//##########################
+
+//public class P
+//{
+//    private String vehicle;
+//    public String getVehicle()
+//    {
+//        return vehicle;
+//    }
+//    public void setVehicle(String Nvehicle)
+//    {
+//        this.vehicle=Nvehicle;
+//    }
+//}
+//class V
+//{
+//    public static void main(String[] args) {
+//        P ob=new P();
+//        ob.setVehicle("Bugatti");
+//        System.out.println(ob.getVehicle());
+//    }
+//}
