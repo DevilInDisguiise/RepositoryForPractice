53b449c7bda65f589ec808ccc8bc9a0a0e2ea5f3
DevilInDisguiise <juusafi@gmail.com>
2024-12-04 15:08:19 -0500

    First commit from new branch

--- src/P.java
+++ src/P.java
@@ -3,85 +3,111 @@ import java.util.Scanner;
 
 //class Vehicle {
 //    protected String brand="Ford";
-//    void carsound()
+//####################################################
+//        Abstract class/method
+//####################################################
+
+//abstract class J{
+//    public abstract void p();
+//    public void p2()
 //    {
-//        System.out.println("honk honk");
+//        System.out.println("This is non-abstract method of J");
 //    }
-//    public void carbuyear()
+//    public void p3()
 //    {
-//        System.out.println("2024");
+//        System.out.println("This is Second non abstract method of J");
 //    }
 //}
-//class Car extends Vehicle{
-//    public void caryear()
+//class K extends J{
+//    public void p()
 //    {
-//        System.out.println("2018");
+//        System.out.println("this is abstract method for class K");
 //    }
-//    public void carsound()
+//    public void p2()
 //    {
-//        System.out.println("poo poo");
+//        System.out.println("This is non abstract method of class K");
 //    }
+//    public void p3()
+//    {
+//        System.out.println("This is Second non abstract method of class K");
+//    }
+//}
+//class L extends J{
+//    public void p()
+//    {
+//        System.out.println("this is abstract method for class L");
+//    }
+//    public void p2()
+//    {
+//        System.out.println("This is non abstract method of class L");
+//    }
+//}
+//public class P{
+//    public staticw void main(String[] args) {
+//        System.out.println("-------------");
+//        K oK=new K();
+//        oK.p();
+//        oK.p2();
+//        oK.p3();
+//        System.out.println("-------------");
+//        J oJ=new K();
+//        oJ.p();
+//        oJ.p2();
+//        oJ.p3();
+//        System.out.println("-------------");
+//        L oL=new L();
+//        oL.p();
+//        oL.p2();
+//        System.out.println("-------------");
+//    }
+//}
+
+
+
+//####################################################
+//        Interface class/method
+//####################################################
+
+//public interface P {
+//    int jaco=12;//interface attributes by default are static
+//    public void pm1();
+//    public void pm2();
+//}
+//class M implements P{
+//    public void pm1() {
+//        System.out.println("First Interface method");
+//    }
+//    public void pm2() {
+//        System.out.println("Second Interface method");
+//    }
+//}
+//class J{
 //    public static void main(String[] args) {
-//        String carmodel="Mustang";
-//        Car mycar=new Car();
-//        mycar.carsound();
-//        System.out.println(mycar.brand+" = "+carmodel);
-//        mycar.carsound();
-//        Vehicle mecar=new Car();
-//        mecar.carbuyear();
-//
+//        M ob=new M();ob.pm1();ob.pm2();
+//        System.out.println(P.jaco);
 //    }
 //}
 
 
 
 
+//####################################################
+//        CONSTRUCTOR
+//####################################################
 
+public class P{
+    int x;
+    int y;
+     public P(int z, int w)
+    {
+        x=z;
+        y=w;
+    }
 
+    public static void main(String[] args) {
 
-//##########################
-//        Arrays
-//##########################
-//class Array{
-//    public static void main(String[] args) {
-//        int[][]  rank={{1,2},{5,3},{4}};
-//        for (int i=0;i<rank.length;i++)
-//        {
-//            for (int j=0;j<rank[i].length;j++)
-//            {
-//                System.out.println(rank[i][j]);
-//            }
-//        }
-//
-//
-//    }
-//}
-
-
-
-
-
-//##########################
-//        Encapsulation
-//##########################
-
-//public class P
-//{
-//    private String vehicle;
-//    public String getVehicle()
-//    {
-//        return vehicle;
-//    }
-//    public void setVehicle(String Nvehicle)
-//    {
-//        this.vehicle=Nvehicle;
-//    }
-//}
-//class V
-//{
-//    public static void main(String[] args) {
-//        P ob=new P();
-//        ob.setVehicle("Bugatti");
-//        System.out.println(ob.getVehicle());
-//    }
-//}
+         P ob=new P(20,30);
+        System.out.println(ob.x);
+        System.out.println(ob.y);
+    }
+}
